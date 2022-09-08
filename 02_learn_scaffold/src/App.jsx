fd216468089b577c18dee0b52440bb47086a0da5
Debbl <me@aiwan.run>
2022-09-08 11:56:43 +0800

    02_init

---
+++ 02_learn_scaffold/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HelloWorld from "./Components/HelloWord";
+
+const App = () => {
+  return (
+    <div>
+      <h2>Hello</h2>
+      <HelloWorld />
+    </div>
+  );
+};
+
+export default App;
